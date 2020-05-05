89e0f8415ce12110de5e0a1b1c8825d3c95dcd04
Mesay Bekele <me2say@gmail.com>
2020-05-05 18:34:54 -0400

    update styling and technology section

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -13,22 +13,22 @@ function AboutMe (props) {
   <p>Web Developer Experienced in Quality Assurance Automation Tester with a demonstrated history of working in the financial services industry. Skilled in Requirements Analysis, manual testing, Agile Methodologies, Test Automation, Selenium, Cucumber, Jira, API testing, SQL and Software Development Life Cycle (SDLC). Strong quality assurance professional with a Bachelor of Science - BS focused in Chemical Engineering from Addis Ababa University. 
 Current Full-Stack Web Development Student At Georgia Tech </p>
   <hr className="hr-light my-4"/>
-<p><strong>JavaScript | HTML | CSS | JQuery | React | Node.js | MongoDB | MySQL</strong></p>
+<p><strong>JavaScript | HTML | CSS | jQuery | React | Express | Node.js  <br/> MongoDB | MySQL</strong></p>
   <hr className="hr-light my-4"/>
           <ul className="list-inline text-center list-unstyled">
             <li className="list-inline-item myIcons">
               <a className="p-2 fa-2x tw-ic" href="https://github.com/mesayb" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github" style={{"color":"white"}}></i>
+                <i className="fab fa-github zoom" style={{"color":"red"}}></i>
               </a>
             </li>
             <li className="list-inline-item myIcons">
               <a className="p-2 fa-2x li-ic" href="https://www.linkedin.com/in/mesay-bekele/" target="_blank" rel="noopener noreferrer" >
-                <i className="fab fa-linkedin-in" style={{"color":"white"}}> </i>
+                <i className="fab fa-linkedin-in zoom" style={{"color":"red"}}> </i>
               </a>
             </li>
             <li className="list-inline-item myIcons">
               <a className="p-2 fa-2x ins-ic" href="/pdfresume">
-                <i className="far fa-file-pdf" style={{"color":"white"}}></i>
+                <i className="far fa-file-pdf zoom" style={{"color":"red"}}></i>
               </a>
             </li>
           </ul>
