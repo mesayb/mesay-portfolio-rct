0e79f4611546afaf3a9f933a6ba662450d7cb560
Mesay Bekele <me2say@gmail.com>
2020-05-05 13:48:22 -0400

    Updates

---
+++ precache-manifest.8fb362e50a31b809d387a74f147d13d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ccacb1ef6f4cb4ca593443d3b4fc1c",
+    "url": "/mesay-portfolio-rct/index.html"
+  },
+  {
+    "revision": "3c8659a8040ddd8766a9",
+    "url": "/mesay-portfolio-rct/static/css/main.31bc7019.chunk.css"
+  },
+  {
+    "revision": "d6add9debaa2a93d9c2e",
+    "url": "/mesay-portfolio-rct/static/js/2.0eff3c74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mesay-portfolio-rct/static/js/2.0eff3c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8659a8040ddd8766a9",
+    "url": "/mesay-portfolio-rct/static/js/main.a6c31382.chunk.js"
+  },
+  {
+    "revision": "7285cf34ef56e727bbf0",
+    "url": "/mesay-portfolio-rct/static/js/runtime-main.d37150b2.js"
+  }
+]);
